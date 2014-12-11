2f8d757e948c2e0343bd6888ba3498bc11cf7511
revisoft <via_783@yahoo.com>
2014-12-11 17:22:03 +0700

    Revert "ini"
    This reverts commit f2f4fd4ee5371b2f946d87a7ea811e50d72bca69.

--- application/views/template/admin/header.php
+++ application/views/template/admin/header.php
@@ -20,8 +20,7 @@
     </style>
     <link href="<?php echo base_url(); ?>assets/admin/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/admin/css/bootstrap-wysihtml5.css" rel="stylesheet">
-	  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/admin/css/table-fixed-header.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/admin/css/styles.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/admin/css/table-fixed-header.css">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
--- application/views/template/admin/sidebar.php
+++ application/views/template/admin/sidebar.php
@@ -2,9 +2,8 @@
 					<div class="span2">
 						<ul class="nav nav-list"><!-- menu with icon -->
 							<li class="nav-header">
-								Menu XMART
+								Menu Template Admin
 							</li>
-							<!--
 							<?php if($this->uri->segment(2) == "produk" || $this->uri->segment(2) == "tambah_produk" || $this->uri->segment(2) == "submit_tambah_produk" || $this->uri->segment(2) == "edit_produk" || $this->uri->segment(2) == "kategori_produk" || $this->uri->segment(2) == "submit_edit_produk" || $this->uri->segment(2) == "edit_kategori_produk" || $this->uri->segment(2) == "submit_edit_kategori_produk" || $this->uri->segment(2) == "submit_tambah_kategori" ) { ?>
 							<li class="active">
 							<?php }else{ ?>
@@ -39,130 +38,9 @@
 							<?php } ?>
 								<a href="<?php echo base_url(); ?>administrator/contact_us/">
 									<i class="icon-comment"></i> Contact Us
-								</a>								
-							</li> -->							
-							<div class="accordion" id="leftMenu">
-		                    <div class="accordion-group">
-		                        <div class="accordion-heading">
-		                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#">
-		                                  <i class="icon-home"></i> Dashboard
-		                            </a>
-		                        </div>
-		                    </div>
-		                    <div class="accordion-group"> 
-		                    	        <div class="accordion-heading">
-				                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#transactions_distribution">
-				                                <i class="icon-th"></i> Transactions Distribution
-				                            </a>
-				                        </div>
-				                        <div id="transactions_distribution" class="accordion-body collapse" style="height: 0px; ">
-				                            <div class="accordion-inner">
-				                                <ul>		                                    
-				                                	<li>Purchase Request</li>
-				                                    <li>Sales Quetation</li>
-				                                    ------------------------
-				                                </ul>
-				                            </div>
-				                        </div>
-				            </div>
-				            <div class="accordion-group">            
-				                        <div class="accordion-heading">
-				                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#transactions_finansial">
-				                                <i class="icon-th"></i> Transactions Finansial
-				                            </a>
-				                        </div>
-				                        <div id="transactions_finansial" class="accordion-body collapse" style="height: 0px; ">
-				                            <div class="accordion-inner">
-				                                <ul>		                                    
-				                                	<li>Purchase Request</li>
-				                                    <li>Sales Quetation</li>
-				                                    ------------------------
-				                                </ul>
-				                            </div>
-				                        </div>
-				                    <!--</div>    -->			                    
-		                    </div>
-		                    <div class="accordion-group">
-		                        <div class="accordion-heading">
-		                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseThree">
-		                                <i class="icon-th-list"></i> Pending Approval
-		                            </a>
-		                        </div>
-		                        <div id="collapseThree" class="accordion-body collapse" style="height: 0px; ">
-		                            <div class="accordion-inner">
-		                                <ul>
-		                                    <li>This is one</li>
-		                                    <li>This is two</li>
-		                                    <li>This is Three</li>
-		                                </ul>                 
-		                            </div>
-		                         </div>
-		                    </div>
-		                    <div class="accordion-group">
-		                        <div class="accordion-heading">
-		                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseFour">
-		                                <i class="icon-list-alt"></i> Process
-		                            </a>
-		                        </div>
-		                        <div id="collapseFour" class="accordion-body collapse" style="height: 0px; ">
-		                            <div class="accordion-inner">
-		                                <ul>
-		                                    <li>This is one</li>
-		                                    <li>This is two</li>
-		                                    <li>This is Three</li>
-		                                </ul>                 
-		                            </div>
-		                         </div>
-		                    </div>
-		                    <div class="accordion-group">
-		                        <div class="accordion-heading">
-		                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseFive">
-		                                <i class="icon-cog"></i> Master
-		                            </a>
-		                        </div>
-		                        <div id="collapseFive" class="accordion-body collapse" style="height: 0px; ">
-		                            <div class="accordion-inner">
-		                                <ul>
-		                                    <li>This is one</li>
-		                                    <li>This is two</li>
-		                                    <li>This is Three</li>
-		                                </ul>                 
-		                            </div>
-		                         </div>
-		                    </div>
-		                    <div class="accordion-group">
-		                        <div class="accordion-heading">
-		                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#collapseSix">
-		                                <i class="icon-file"></i> Setup
-		                            </a>
-		                        </div>
-		                        <div id="collapseSix" class="accordion-body collapse" style="height: 0px; ">
-		                            <div class="accordion-inner">
-		                                <ul>
-		                                    <li>This is one</li>
-		                                    <li>This is two</li>
-		                                    <li>This is Three</li>
-		                                </ul>                 
-		                            </div>
-		                         </div>
-		                    </div>
-		                    <div class="accordion-group">
-		                        <div class="accordion-heading">
-		                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#leftMenu" href="#report">
-		                                <i class="icon-file"></i> Report
-		                            </a>
-		                        </div>
-		                        <div id="report" class="accordion-body collapse" style="height: 0px; ">
-		                            <div class="accordion-inner">
-		                                <ul>
-		                                    <li>This is one</li>
-		                                    <li>This is two</li>
-		                                    <li>This is Three</li>
-		                                </ul>                 
-		                            </div>
-		                         </div>
-		                    </div>
-		                </div>
+								</a>
+							</li>
+							
 						</ul><!-- end menu with icon -->
 						<hr />
 					</div>
